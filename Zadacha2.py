f3cb5925c93c4fb37c52ee2284037218b83f2aa6
Haru <harumolodec@mail.ru>
2022-10-01 02:19:41 +0700

    Zadacha2 END

---
+++ Zadacha2.py
@@ -0,0 +1,38 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+
+from random import sample
+
+def num_list(count):
+    if count < 0:
+        return "Error"
+    list1 = sample(range(1, 10), count)
+    print('Список элементов:', list1)
+    list2 = list()
+    for i in range(count // 2):
+        gen = list1[i] * list1[(i + 1) * -1]
+        list2.append(gen)
+    print('Произведение пар чисел:', list2)
+
+num_list(int(input('Укажите количество элементов в списке: ')))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
