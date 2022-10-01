397c8c6e3961846e1d9ddb2a9ab14c8703ae1abd
Haru <harumolodec@mail.ru>
2022-10-01 14:27:50 +0700

    Zadacha5 END

--- Zadacha5.py
+++ Zadacha5.py
@@ -1,20 +1,38 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 def fib(num):
+    a, b = 1, 1
     fib_list = [1, 0, 1]
     if num <= 0:
         return print('Ошибка! Повторите запрос!')
-    elif num == 1:
-        print('Список чисел:', fib_list)
-    else:
-        i = 3
-        while i < num * 2:
-            negofib_list = fib_list[i - 1] + fib_list[i - 2]
-            fib_list.append(negofib_list)
-            if len(fib_list) % 4 == 0:
-                negofib_list = -fib_list[i] 
-            fib_list.insert(0, negofib_list)
-            i += 2
-        print('Список чисел:', fib_list)
-
+    for i in range(1, num):
+        a, b = b, a + b
+        fib_list.append(a)
+        fib_list.insert(0, a * (-1) ** i)
+    print('Список чисел:', fib_list)
 fib(int(input('Укажите размер списка: ')))
+
+
+
+# def fib(num):
+#     fib_list = [1, 0, 1]
+#     if num <= 0:
+#         return print('Ошибка! Повторите запрос!')
+#     elif num == 1:
+#         print('Список чисел:', fib_list)
+#     else:
+#         i = 3
+#         while i < num * 2:
+#             negofib_list = fib_list[i - 1] + fib_list[i - 2]
+#             fib_list.append(negofib_list)
+#             if len(fib_list) % 4 == 0:
+#                 negofib_list = -fib_list[i] 
+#             fib_list.insert(0, negofib_list)
+#             i += 2
+#         print('Список чисел:', fib_list)
+
+
+
+# fib(int(input('Укажите размер списка: ')))
+
+
