097491fdabb7dadbf55a31de594dec5af0a815d5
Haru <harumolodec@mail.ru>
2022-10-01 14:04:57 +0700

    Zadacha5 END

--- Zadacha5.py
+++ Zadacha5.py
@@ -1,24 +1,20 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 def fib(num):
+    fib_list = [1, 0, 1]
     if num <= 0:
-        return 'Ошибка! Повторите запрос!'
-
-    fib_list = [0, 1]
-
-    for i in range(1, num):
-        fib_list.append(fib_list[i] + fib_list[i - 1])
-
-    negofib_list = fib_list[::-1]
-
-    for i in range(0, num - 1, 2):
-        negofib_list[i] = -negofib_list[i]
-
-    negofib_list.pop(num)
-    print('Список чисел:', negofib_list + fib_list)
+        return print('Ошибка! Повторите запрос!')
+    elif num == 1:
+        print('Список чисел:', fib_list)
+    else:
+        i = 3
+        while i < num * 2:
+            negofib_list = fib_list[i - 1] + fib_list[i - 2]
+            fib_list.append(negofib_list)
+            if len(fib_list) % 4 == 0:
+                negofib_list = -fib_list[i] 
+            fib_list.insert(0, negofib_list)
+            i += 2
+        print('Список чисел:', fib_list)
 
 fib(int(input('Укажите размер списка: ')))
-
-
-
-
